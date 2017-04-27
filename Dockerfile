cbd1b0b08c7b19767a53f2cad9815d40f92c90c2
Josh <josh.young11@gmail.com>
2017-04-27 22:09:32 +0000

    Fixed all of the ansible files and changed port in flask file to 5000

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,4 @@ RUN pip3 install Flask
 COPY . /src
 WORKDIR /src
 
-EXPOSE 8080
-
 RUN python3 unh698_test.py
--- unh698.py
+++ unh698.py
@@ -10,4 +10,4 @@ def computer():
     return render_template('computer.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=8080)
+    app.run(debug=True,host='0.0.0.0', port=5000)
